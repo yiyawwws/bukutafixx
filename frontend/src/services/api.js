99c38fd781fccf0d5c5a2eddddcd04e1e5a59721
yiyawwws <ainowaifuu10@gmail.com>
2026-07-13 03:12:19 +0800

    Hardcode API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
+  baseURL: 'https://humvee-pawing-coastline.ngrok-free.dev/api',
   timeout: 30000, // 30 detik timeout untuk mengakomodasi upload file ke Cloudinary
   headers: {
     'ngrok-skip-browser-warning': 'true'
